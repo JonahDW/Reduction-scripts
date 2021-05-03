2822eb7737d728856d7c71ab24066e991b969df0
Jonah Wagenveld <wagenveld@pc002.mpifr-bonn.mpg.de>
2021-05-03 12:16:02 +0200

    Update flags

--- 2GC/SCRIPTS/flagging.py
+++ 2GC/SCRIPTS/flagging.py
@@ -1,11 +1,43 @@
 from sys import argv
 msfile = argv[3]
 
+# Bad frequencies
+flagdata(vis=vis,
+         mode='manual',
+         spw="""*:856~890MHz,
+                *:924~970MHz,
+                *:1027~1033MHz,
+                *:1039~1044MHz,
+                *:1090~1095MHz,
+                *:1139~1310MHz,
+                *:1415~1425MHz,
+                *:1510~1630MHz""")
+
+# UVrange
+flagdata(vis=vis,
+         mode='manual',
+         uvrange = '<600')
+
+# Auto correlations
+flagdata(vis=vis,
+         mode='manual',
+         autocorr=True)
+
+# Zeros
+flagdata(vis=vis,
+         mode='clip',
+         clipzeros=True)
+
+# Clip
+flagdata(vis=vis,
+         mode='clip',
+         clipminmax=[0.0,100.0])
+
 flagmanager(vis=msfile,
             mode='save',
-            versionname='default')
+            versionname='basic')
 
-flagdata(vis=msfile, 
-         mode='tfcrop',
+flagdata(vis=msfile,
+         mode='rflag',
          action='apply',
          display='')
